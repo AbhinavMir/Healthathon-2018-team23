29d3e2ca2c63902b24099e038a46e791fc107dde
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2018-10-05 04:38:39 +0530

    2

---
+++ land.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title><!--Enter title here for later--></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+  <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"><!--Let browser know website is optimized for mobile-->
+
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+
+      <link rel="stylesheet" href="land.css">
+
+</head>
+<body>
+      <div class="container">
+          <div class="navbar row align-center">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+          <div class="col l8 s8 m7">
+            <img src="neure2.png" alt="logo" style="width:350px;height:150px;">
+    <!--<h2 class="header"><b>neure</b></h2>-->
+    <h3>The website neure′ helps you keep all your medical records updated from your concerned doctors and hospitals.<h3>
+
+      <a class="waves-effect waves-light btn btn-large" href="login.php">Login</a>
+      <a class="waves-effect waves-light btn btn-large" href="register.php">Signup</a>
+      </div>
+    </div>
+    </div>
+
+
+      </body>
+       </html>
